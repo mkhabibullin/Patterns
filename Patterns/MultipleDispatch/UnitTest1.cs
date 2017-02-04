0ad40b77111134d5735a3556764dad8dca1c892e
Марат Хабибуллин <khabibullin@hightech.today>
2017-02-04 15:50:35 +0300

    multidispatch

---
+++ Patterns/MultipleDispatch/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultipleDispatch
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var asteroid = new Asteroid();
+            var spaceship = new Spaceship();
+            CollideWith(asteroid, spaceship);
+            CollideWith(spaceship, spaceship);
+        }
+    }
+
+    public class Program
+    {
+        class Thing
+        {
+        }
+
+        class Asteroid : Thing
+        {
+        }
+
+        class Spaceship : Thing
+        {
+        }
+
+        static void CollideWithImpl(Asteroid x, Asteroid y)
+        {
+            Console.WriteLine("Asteroid collides with Asteroid");
+        }
+
+        static void CollideWithImpl(Asteroid x, Spaceship y)
+        {
+            Console.WriteLine("Asteroid collides with Spaceship");
+        }
+
+        static void CollideWithImpl(Spaceship x, Asteroid y)
+        {
+            Console.WriteLine("Spaceship collides with Asteroid");
+        }
+
+        static void CollideWithImpl(Spaceship x, Spaceship y)
+        {
+            Console.WriteLine("Spaceship collides with Spaceship");
+        }
+
+        static void CollideWith(Thing x, Thing y)
+        {
+            dynamic a = x;
+            dynamic b = y;
+            Program.CollideWithImpl(a, b);
+        }
+    }
+}
---
+++ Patterns/MultipleDispatchConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleDispatchConsole
+{
+    class Program
+    {
+        class Thing { }
+        class Asteroid : Thing { }
+        class Spaceship : Thing { }
+
+        static void CollideWithImpl(Asteroid x, Asteroid y)
+        {
+            Console.WriteLine("Asteroid collides with Asteroid");
+        }
+
+        static void CollideWithImpl(Asteroid x, Spaceship y)
+        {
+            Console.WriteLine("Asteroid collides with Spaceship");
+        }
+
+        static void CollideWithImpl(Spaceship x, Asteroid y)
+        {
+            Console.WriteLine("Spaceship collides with Asteroid");
+        }
+
+        static void CollideWithImpl(Spaceship x, Spaceship y)
+        {
+            Console.WriteLine("Spaceship collides with Spaceship");
+        }
+
+        static void CollideWith(Thing x, Thing y)
+        {
+            dynamic a = x;
+            dynamic b = y;
+            CollideWithImpl(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            var asteroid = new Asteroid();
+            var spaceship = new Spaceship();
+            CollideWith(asteroid, spaceship);
+            CollideWith(spaceship, spaceship);
+
+            Console.Read();
+        }
+    }
+}

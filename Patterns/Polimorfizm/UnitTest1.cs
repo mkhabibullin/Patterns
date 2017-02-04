f9ca52118fefc9e61f4fc73884b7ec89087b5a6b
Марат Хабибуллин <khabibullin@hightech.today>
2017-02-04 16:34:59 +0300

    polimorf

---
+++ Patterns/Polimorfizm/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Polimorfizm
+{
+    using System.Diagnostics;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            B b = new B();
+            A a = b;
+
+            a.F();
+            b.F();
+
+            a.G();
+            b.G();
+
+            /*
+            A.F
+            B.F
+
+            B.G
+            B.G
+             */
+        }
+    }
+
+    public class A
+    {
+        public void F()
+        {
+            Trace.WriteLine("A.F");
+        }
+
+        public virtual void G()
+        {
+            Trace.WriteLine("A.G");
+        }
+    }
+
+    public class B : A
+    {
+        public void F()
+        {
+            Trace.WriteLine("B.F");
+        }
+
+        public override void G()
+        {
+            Trace.WriteLine("B.G");
+        }
+    }
+}

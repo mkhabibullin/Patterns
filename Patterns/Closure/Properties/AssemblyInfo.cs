353499c3e8196a21aa544e23eb33aa4023f37220
mkhabibullin <safe.kzn@gmail.com>
2017-03-26 21:19:23 +0300

    Closure

---
+++ Patterns/Closure/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Closure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Closure")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c3d0c0c-e49b-4d99-8537-859aa19c3272")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Patterns/Closure/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Closure
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string myVar = " this is good";
+
+        [TestMethod]
+        public void TestDelegateClosure1()
+        {
+            Func<string, string> myFunc = delegate (string var1)
+            {
+                var resultConcat = var1 + myVar;
+                myVar = "";
+                return resultConcat;
+            };
+
+            var result = myFunc("Uran");
+            Trace.WriteLine(result);
+
+            var result2 = myFunc("Uran 237");
+            Trace.WriteLine(result2);
+        }
+
+        [TestMethod]
+        public void TestDelegateClosure2()
+        {
+            var actions = new List<Action>();
+
+            for(var i = 0; i < 3; i ++)
+            {
+                //var tempI = i;
+                actions.Add(() => Trace.WriteLine(i));
+            }
+
+            foreach(var a in actions)
+            {
+                a.Invoke();
+            }
+        }
+    }
+}

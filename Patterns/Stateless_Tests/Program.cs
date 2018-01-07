4cd48ea82c459b5d5ec029c8d4602e4fc4d2e412
KhabibullinM <marat-011@yandex.ru>
2018-01-07 22:02:51 +0300

    Stateless

---
+++ Patterns/Stateless_Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stateless_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

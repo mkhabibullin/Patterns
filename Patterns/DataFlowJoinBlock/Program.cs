b9028933e3f75993f9e46b5370d00aabc1ceac23
Marat Khabibullin <marat_khabibullin@epam.com>
2022-01-05 20:42:48 +0300

    DataFlow JoinBlock

---
+++ Patterns/DataFlowJoinBlock/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace DataFlowJoinBlock
+{
+    class Program
+    {
+        static BufferBlock<string> FirstSource = new BufferBlock<string>();
+        static BufferBlock<string> SecondSource = new BufferBlock<string>();
+
+        static void Main(string[] args)
+        {
+            var joinTwoSourceBlock = new JoinBlock<string, string>(
+                new GroupingDataflowBlockOptions
+                {
+                    Greedy = false
+                });
+
+            var actionOverTwoResources = new ActionBlock<Tuple<string, string>>(
+                data =>
+                {
+                    Console.WriteLine($"Two resources are gotten: \n1 - \"{data.Item1}\" \n2 - \"{data.Item2}\".");
+
+                    Console.WriteLine("Starting the work over resources ...");
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                    Console.WriteLine("End of the work.");
+                });
+
+            FirstSource.LinkTo(joinTwoSourceBlock.Target1);
+            SecondSource.LinkTo(joinTwoSourceBlock.Target2);
+
+            joinTwoSourceBlock.LinkTo(actionOverTwoResources);
+
+            FirstSource.Post("1 - A");
+            FirstSource.Post("1 - B");
+            FirstSource.Post("1 - C");
+
+            Task.Run(SecondSourceGenerator);
+            Task.Run(PrintGreedyEffect);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static void SecondSourceGenerator()
+        {
+            while (true)
+            {
+                SecondSource.Post(Console.ReadLine());
+            }
+        }
+
+        static void PrintGreedyEffect()
+        {
+            while (true)
+            {
+                //  In greedy mode, if one join block reads from the first source and the second join block reads from the second source,
+                //  the application might deadlock because both join blocks mutually wait for the other to release its resource.
+                //  In non-greedy mode, each join block reads from its sources only when all data is available, and therefore,
+                //  the risk of deadlock is eliminated.
+                Console.WriteLine($"Buffered items: {FirstSource.Count}");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
---
+++ Patterns/DataFlowSandbox2/Program.cs
@@ -0,0 +1,112 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace DataFlowSandbox2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseDataFlow.Process(
+                "1 2 3 4 5 6 7 8 9 0 11 22 33 44 55 66 77 88 99 00",
+                Environment.ProcessorCount);
+
+            //BenchmarkRunner.Run<Benchmark>();
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+
+    public static class ParseDataFlow
+    {
+        public static void Process(
+            string input,
+            int maxDegreeOfParallelism,
+            bool logging = true,
+            TimeSpan? delay = null)
+        {
+            var parseBlock = new TransformManyBlock<string, string>(input =>
+            {
+                return input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            });
+
+            var manualBlock = new TransformManyBlock<string, string>(ConsoleSource);
+
+            var timerBlock = new TransformManyBlock<int, string>(TimerSource);
+
+            var processBlock = new ActionBlock<string>(async token =>
+            {
+                if (delay != null)
+                {
+                    await Task.Delay(delay.Value);
+                }
+                if (logging)
+                {
+                    Console.WriteLine($"Token: {token}, Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+                }
+            },
+            new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxDegreeOfParallelism
+            });
+
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+            parseBlock.LinkTo(processBlock, linkOptions);
+            manualBlock.LinkTo(processBlock, linkOptions);
+            timerBlock.LinkTo(processBlock, linkOptions);
+
+            parseBlock.Post(input);
+            manualBlock.Post("");
+            timerBlock.Post(5000);
+
+            parseBlock.Completion.Wait();
+        }
+
+        private static IEnumerable<string> ConsoleSource(string _)
+        {
+            while(true)
+            {
+                yield return Console.ReadLine();
+            }
+        }
+
+        private static IEnumerable<string> TimerSource(int delay)
+        {
+            var i = 0;
+            while (true)
+            {
+                Thread.Sleep(delay);
+                yield return (i++).ToString();
+            }
+        }
+    }
+
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    [SimpleJob(launchCount: 1, warmupCount: 3, targetCount: 10)]
+    public class Benchmark
+    {
+        [Benchmark]
+        public void NoParallel() => ParseDataFlow.Process(
+            input: "1 2 3 4 5 6 7 8 9 0 11 22 33 44 55 66 77 88 99 00",
+            maxDegreeOfParallelism: 1,
+            logging: false,
+            delay: TimeSpan.FromMilliseconds(100));
+
+        [Benchmark]
+        public void Parallel() => ParseDataFlow.Process(
+            input: "1 2 3 4 5 6 7 8 9 0 11 22 33 44 55 66 77 88 99 00",
+            maxDegreeOfParallelism: Environment.ProcessorCount,
+            logging: false,
+            delay: TimeSpan.FromMilliseconds(100));
+    }
+}
